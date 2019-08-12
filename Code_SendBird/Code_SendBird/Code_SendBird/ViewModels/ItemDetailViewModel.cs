e65bf9d7ece18bf28e7f435c6874769db9221ac9
Huu Bao Nguyen <baonh@omnicasa.com>
2019-08-12 11:00:10 +0700

    commit

---
+++ Code_SendBird/Code_SendBird/Code_SendBird/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Code_SendBird.Models;
+
+namespace Code_SendBird.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
